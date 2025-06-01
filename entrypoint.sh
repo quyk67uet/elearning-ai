f9a88f043fa949909fd2e9d66f5fda4d8da8e516
quyk67uet <22028190@vnu.edu.vn>
2025-06-02 00:03:04 +0700

    Update new 32

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+mkdir -p /app/elearning-bench/sites/learn.local/logs
+
 # Tạo file ca.pem từ environment variable
 if [ -n "$CA_PEM_CONTENT" ]; then
     echo "$CA_PEM_CONTENT" > /app/elearning-bench/sites/learn.local/ca.pem
